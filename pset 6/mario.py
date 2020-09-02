8067dc16f4f96f7286fa9f02eecd84104454b0be
Abhimanyu Karki <63642063+Abhikarki@users.noreply.github.com>
2020-09-02 22:59:01 +0545

    Add files via upload

---
+++ pset 6/mario.py
@@ -0,0 +1,13 @@
+height = int(input("Height: "))
+
+#prompt the user for a positive number.
+while height <= 0 or height > 8:
+    height = int(input("Height: "))
+
+for i in range(1, height + 1):
+    j = height - i
+    #To avoid printing empty line at the end.
+    if i == height:
+       print(" " * j + "#" * i , end ='') 
+       continue
+    print(" " * j + "#" * i)
