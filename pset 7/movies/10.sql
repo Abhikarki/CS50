9c8167a9bf6128c06037022d7874b41b894db0f1
Abhimanyu Karki <63642063+Abhikarki@users.noreply.github.com>
2020-09-10 19:03:37 +0545

    Add files via upload

---
+++ pset 7/movies/10.sql
@@ -0,0 +1,7 @@
+-- list the names of all people who have directed a movie that received a rating of at least 9.0
+
+SELECT name FROM people WHERE id IN(
+    SELECT directors.person_id FROM directors
+    JOIN ratings
+    ON directors.movie_id = ratings.movie_id
+    WHERE ratings.rating >= 9.0);
